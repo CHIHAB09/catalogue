82aba8fb422424952a751bd679ee573f5bfbfe3c
CHIHAB09 <web2020.chihab@gmail.com>
2020-10-08 16:35:55 +0200

    premiere etape projet catalogue

---
+++ public/config.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database
+define("DB_HOST","localhost");
+define("DB_NAME","catalogue");
+define("DB_USER","root");
+define("DB_PWD","");
+define("DB_PORT",3306);
+define("DB_CHARSET","utf8");
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+// mettre un switch
+//cree les appel des config et des autres dossier ou fichier
