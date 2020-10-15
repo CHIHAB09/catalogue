e3d60632cbdc2a5a9cf6210aada7df35f56e139c
CHIHAB09 <web2020.chihab@gmail.com>
2020-10-15 16:47:55 +0200

    ajout des requetes

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
-$db= connectDB();
 
+require_once "public/config.php";
 
+//connexion à la db.
+$db= mysqli_connect(DB_HOST,DB_USER,DB_PWD,DB_NAME,DB_PORT) OR die ("Echec de la connexion");
+
+// initialisation de caractere a la db.
+mysqli_set_charset($db,DB_CHARSET);
+require_once "model/crud.php";
 
 
 
--- model/crud.php
+++ model/crud.php
@@ -1,7 +1,51 @@
 <?php
 
-function selectNom($db){
-    $sql="SELECT 'modele' FROM catalogue";
+// ----------> CREATE <--------------
+
+//cree un produit
+function insertProduit($db,$modele,$top_vente,$marque,$descriptif,$prix){
+    $sql= "INSERT INTO produits (modele, top_vente, marque, descriptif, prix) VALUES('$modele','$top_vente','$marque','$descriptif','$prix');";
+    $result = mysqli_query($db, $sql);
+	return $result ? "L'insertion a réussie<br>" : "L'insertion a échouée: " . mysqli_error($db) . "<br>";
+}
+//var_dump(insertProduit($db,'Heart Bio Hack','0','Puma','lorem ipsum', '58.50'));
+
+//cree une categorie
+function insertCategorie($db,$nom){
+    $sql= "INSERT INTO categorie (nom) VALUES('$nom');";
+    $result = mysqli_query($db, $sql);
+	return $result ? "L'insertion a réussie<br>" : "L'insertion a échouée: " . mysqli_error($db) . "<br>";
+}
+
+//cree une image
+function insertImages($db,$legend,$URL,$produit_idproduit){
+    $sql= "INSERT INTO images (legend,URL,produits_idproduit) VALUES('$legend','$URL','$produit_idproduit');";
+    $result = mysqli_query($db, $sql);
+	return $result ? "L'insertion a réussie<br>" : "L'insertion a échouée: " . mysqli_error($db) . "<br>";
+}
+
+// cree un point de vente
+function insertMagasin($db,$nom,$rue,$numero,$codepostal,$ville,$coordonee){
+    $sql= "INSERT INTO poi,t_de_vente (nom,rue,numero,codepostal,ville,coordonnee) VALUES('$nom','$rue','$numero','$codepostal','$ville','$coordonee');";
+    $result = mysqli_query($db, $sql);
+	return $result ? "L'insertion a réussie<br>" : "L'insertion a échouée: " . mysqli_error($db) . "<br>";
+}
+
+
+
+
+
+
+
+
+
+
+// ----------> READ <--------------
+
+
+// affichage classé par nom
+function selectsNoms($db){
+    $sql="SELECT * FROM produits ORDER BY nom ASC";
     $result = mysqli_query($db, $sql);
     if($result) {
         $data = mysqli_fetch_all($result, MYSQLI_ASSOC);;
@@ -11,9 +55,196 @@ function selectNom($db){
     }
 }
 
+// affichage classé par prix
+function selectsprix($db){
+    $sql="SELECT * FROM produits ORDER BY prix ASC";
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $data;
+    } else {
+        return "La sélection a échouée: " . mysqli_error($db) . "<br>";
+    }
+}
+// affichage des produits classé par categorie
+function selectsCategories($db){
+    $sql="SELECT * 
+    FROM produits P 
+    JOIN produits_has_categorie AS PHC ON P.id= PHC.produits_id 
+    JOIN categorie AS C ON C.idcategorie = PHC.categorie_id ORDER BY C.nom ; ";
+
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $data;
+    } else {
+        return "La sélection a échouée: " . mysqli_error($db) . "<br>";
+    }
+}
+//var_dump(selectsCategories($db));
+
+// affichage les produits d'une categorie
+function selectCategorie($db,$id){
+    $sql="SELECT *
+    FROM produits AS P 
+    JOIN produits_has_categorie ON produits_id  = P.id JOIN categorie AS C ON categorie_id = idcategorie  WHERE idcategorie= $id; ";
+
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $data;
+    } else {
+        return "La sélection a échouée: " . mysqli_error($db) . "<br>";
+    }
+}
+//var_dump(selectCategorie($db,5));
+
+//affichage en fonction du between
+function selectsEntre($db){
+    $sql="SELECT * FROM produits WHERE prix BETWEEN  50 AND 60";
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        return $data;
+    } else {
+        return "La sélection a échouée: " . mysqli_error($db) . "<br>";
+    }
+}
+//var_dump((selectsEntre($db)));
+
+//affichage d'un produit
+function selectNom($db,$id){
+    $sql="SELECT descriptif FROM produits WHERE id = $id";
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $data = mysqli_fetch_assoc($result);
+        return $data;
+    } else {
+        return "La sélection a échouée: " . mysqli_error($db) . "<br>";
+    }
+}
+//var_dump(selectNom($db,2));
+
+//affichage le nombre de produit que je possede
+function selectCount($db){
+    $sql= "SELECT COUNT(*) AS nb FROM produits";
+    $recup=mysqli_query($db,$sql);
+    $data=mysqli_fetch_assoc($recup);
+    return $data;
+}
+//var_dump(selectCount($db));
+
+//affichage le nombre de produit d'une categorie
+function selectCountCat($db,$id){
+    $sql= "SELECT COUNT(*) AS nb FROM produits JOIN produits_has_categorie ON produits_id = produits.id JOIN categorie ON categorie_id = idcategorie WHERE idcategorie = $id";
+    $recup=mysqli_query($db,$sql);
+    $data=mysqli_fetch_assoc($recup);
+    return $data['nb'];
+}
+//var_dump(selectCountCat($db,3));
+
+//affichage le nombre d'image en fonction de produit
+function selectALLCountImage($db,$id){
+    $sql= "SELECT COUNT(*) AS nbImage FROM produits JOIN images ON produits_idproduit = idproduit WHERE idproduit=$id";
+    $recup=mysqli_query($db,$sql);
+    $data=mysqli_fetch_assoc($recup);
+    return $data ['nbImage'];
+}
+var_dump(selectAllCountImage($db,1));
+// affichage de toute la categorie
+function selectAllCategories($db){
+    $sql="SELECT * FROM categorie ORDER BY nom ASC";
+    $result = mysqli_query($db, $sql);
+    if($result) {
+        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);;
+        return $data;
+    } else {
+        return "La sélection a échouée: " . mysqli_error($db) . "<br>";
+    }
+}
+
+//affichage de la liste des pointes de vente
+function selectsPoint ($db){
+    $sql="SELECT * FROM point_de_vente ORDER BY ville ASC";
+    $result= mysqli_query($db,$sql);
+    if($result){
+        $data= mysqli_fetch_all($result,MYSQLI_ASSOC);
+        return $data;
+    }else{
+        return "Le point de vente n'éxiste plus:" . mysqli_error($db) . "<br>";
+    }
+}
+
+// ----------> UPDATE <--------------
+
+// modification du produit
+function updateProduit($db, $modele, $marque,$id) {
+	$sql = "UPDATE produits SET modele = $modele, marque = $marque WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La mise à jour a réussie<br>" : "La mise à jour a échouée: " . mysqli_error($db) . "<br>";
+}
+
+// modification d'une cathegorie
+function updateCategorie($db, $nom,$id) {
+	$sql = "UPDATE categorie SET nom = $nom WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La mise à jour a réussie<br>" : "La mise à jour a échouée: " . mysqli_error($db) . "<br>";
+}
+
+// modification d'une image
+function updateImage($db, $legend,$URL,$id) {
+	$sql = "UPDATE images SET legend = $legend, URL = $URL WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La mise à jour a réussie<br>" : "La mise à jour a échouée: " . mysqli_error($db) . "<br>";
+}
+
+// modification d'un point de vente
+function updateMagasin($db, $nom,$id) {
+	$sql = "UPDATE point_de_vente SET nom = $nom WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La mise à jour a réussie<br>" : "La mise à jour a échouée: " . mysqli_error($db) . "<br>";
+}
 
 
+// ----------> DELETE <--------------
 
 
+// supprimer un produit
+function deleteProduit($db, $id) {
+	$sql = "DELETE FROM produits WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La suppression a réussi<br>" : "La suppression a raté: " . mysqli_error($db) . "<br>";
+}
+
+//supprimer une categorie
+function deleteCategorie($db, $id) {
+	$sql = "DELETE FROM posts WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La suppression a réussi<br>" : "La suppression a raté: " . mysqli_error($db) . "<br>";
+}
+
+//supprimer une image
+
+function deleteImage($db, $id) {
+	$sql = "DELETE FROM images WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La suppression a réussi<br>" : "La suppression a raté: " . mysqli_error($db) . "<br>";
+}
+
+
+//supprimer un point de vente
+function deletePost($db, $id) {
+	$sql = "DELETE FROM point_de_vente WHERE id = $id";
+	
+	$result = mysqli_query($db, $sql);
+	return $result ? "La suppression a réussi<br>" : "La suppression a raté: " . mysqli_error($db) . "<br>";
+}
 
 ?>
